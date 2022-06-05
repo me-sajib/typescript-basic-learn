d9f5127ca83d575c90d2b167cdfb544e4106a76d
sajib <me.mrsajib@gmail.com>
2022-06-05 18:29:31 +0600

    type checking function

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,18 @@
-let msg = "hello world";
-console.log(msg);
+// type check
+function addData(obj: Person): Person {
+  obj.age += 2;
+  return obj;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+const data: Person = {
+  name: "Max",
+  age: 11,
+};
+const value = addData(data);
+value.name = "sajib";
+console.log(value);
